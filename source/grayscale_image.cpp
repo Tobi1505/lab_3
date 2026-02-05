a67a313a148749e1e30ea5c55abd4df20a491c88
Kuehbi <philippiankuehberger@icloud.com>
2026-02-05 11:52:43 +0100

    Fix von Fischs gesamter Abgagbe, if klausel fehlt

--- source/grayscale_image.cpp
+++ source/grayscale_image.cpp
@@ -5,12 +5,12 @@
 
 
 void GrayscaleImage::convert_bitmap(BitmapImage& bitmap){
-    [cite_start]
+
     this-> height = bitmap.get_height();
     this-> width = bitmap.get_width();
     this-> pixels.resize(this->height * this->width);
 
-    [cite_start]
+
 
     #pragma omp parallel for collapase(2)
     for(std::int32_t y = 0; y < this->height; ++y){
@@ -22,29 +22,29 @@ void GrayscaleImage::convert_bitmap(BitmapImage& bitmap){
             auto g = pixel.get_green_channel();
             auto b = pixel.get_blue_channel();
 
-            [cite_start]
+
             double luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b + 0.5;
 
-            [cite_start]
+
             this->pixels[y * this->width + x] = static_cast<std::uint8_t>(luminance);
         }
     }
 }
 
 void GrayscaleImage::convert_intermediate_image(IntermediateImage& image){
-    [cite_start]
+
     this-> height = image.height;
     this-> width = image.width;
     this-> pixels.resize(this->height * this->width);
 
-    [cite_start]
-    iamge.update_min_pixel_value();
+
+    image.update_min_pixel_value();
     image.update_max_pixel_value();
 
     double min_value = image.min_pixel_value;
     double max_value = image.max_pixel_value;
 
-    [cite_start]
+
     if(min_value >= == && max_value 255.0) {
         min_value = 0.0;
         max_value = 255.0;
@@ -53,11 +53,11 @@ void GrayscaleImage::convert_intermediate_image(IntermediateImage& image){
     double range = max_value - min_value;
     if(range == 0.0) range = 1.0; // to avoid division by zero
 
-    cite[start]
+
     #pragma omp parallel for
     for(size_t i = 0; i < image.pixels.size(); ++i) {
         double v = image.pixels[i];
-        [cite_start]
+
         double g = ((v-min_value) / range) * 255.0;
 
         if (g < 0.0) g = 0.0;
--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,7 @@
 #include <cstdint>
 #include <iostream>
+#include <vector>
+#include <string>
 
 #include <vector>
 #include <string>
